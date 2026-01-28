05e5064bd6ecc23a5f7e75e270e3ca7864317135
Alexandre Hurel <alexandre.hurel@etu.sorbonne-universite.fr>
2026-01-28 17:39:53 +0100

    TP1 fin de séance

--- config_TP1.py
+++ config_TP1.py
@@ -5,7 +5,7 @@ import arenas
 # general -- first three parameters can be overwritten with command-line arguments (cf. "python tetracomposibot.py --help")
 
 display_mode = 0
-arena = 1
+arena = 0
 position = False 
 max_iterations = 501 #401*500
 
@@ -21,10 +21,33 @@ display_time_stats = True
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
 import robot_dumb
+import robot_braitenberg_avoider
+import robot_braitenberg_loveWall
+import robot_braitenberg_loveBot
+import robot_braitenberg_hateWall
+import robot_braitenberg_hateBot
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
-    #x_center = arena_size // 2 - particle_box / 2
+    x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+
+    #braitenberg_hateBot
+    robots.append(robot_braitenberg_hateBot.Robot_player(8, y_center, 0, name="My Robot", team="A"))
+    robots.append(robot_braitenberg_loveBot.Robot_player(6, y_center+2, 0, name="My Robot", team="A"))
+    #braintenberg_loveBoot
+    #robots.append(robot_braitenberg_loveBot.Robot_player(8, y_center, 0, name="My Robot", team="A"))
+    #robots.append(robot_braitenberg_loveBot.Robot_player(8, y_center+4, 0, name="My Robot", team="A"))
+
+    #braintenberg_hateWall
+    #robots.append(robot_braitenberg_hateWall.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    #robots.append(robot_braitenberg_hateWall.Robot_player(9, y_center, 0, name="My Robot", team="A"))
+
+    #braintenberg_loveWall
+    #robots.append(robot_braitenberg_loveWall.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    #robots.append(robot_braitenberg_loveWall.Robot_player(9, y_center, 0, name="My Robot", team="A"))
+
+    #braitenberg_avoider
+    #robots.append(robot_braitenberg_avoider.Robot_player(x_center,y_center,-1,name="Dumb", team="B"))
+    #robots.append(robot_braitenberg_avoider.Robot_player(4, y_center, 0, name="My Robot", team="A"))
     return robots
